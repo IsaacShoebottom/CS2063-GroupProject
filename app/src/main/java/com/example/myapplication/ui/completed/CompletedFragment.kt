f6015b9621e3fa1fd1a6b911d1a8cc06d2079a60
Ethan Pearson <60271460+magmajp@users.noreply.github.com>
2022-10-25 15:29:37 -0300

    completed bottom bar

--- app/src/main/java/com/example/myapplication/ui/completed/CompletedFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/completed/CompletedFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.dashboard
+package com.example.myapplication.ui.completed
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.myapplication.databinding.FragmentDashboardBinding
+import com.example.myapplication.databinding.FragmentCompletedBinding
 
-class DashboardFragment : Fragment() {
+class CompletedFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentCompletedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class DashboardFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+                ViewModelProvider(this).get(CompletedViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentCompletedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textCompleted
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/myapplication/ui/compressing/CompressingViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/compressing/CompressingViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.myapplication.ui.dashboard
+package com.example.myapplication.ui.compressing
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class CompressingViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is compressing Fragment"
     }
     val text: LiveData<String> = _text
 }
